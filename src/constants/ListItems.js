7583655ea8382a34adc094b1b2d2c1c59f51f781
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-01-30 16:00:58 +0530

    constant items file

---
+++ src/constants/ListItems.js
@@ -0,0 +1,7 @@
+export const listItems = [
+    {index: 0, content: "Integrations to bring in context from Zoho, Shopify, etc."},
+    {index: 1, content: "Pre-built templates to send updates & reminders"},
+    {index: 2, content: "Powerful automations to resolve issues faster"},
+    {index: 3, content: "Targeted Campaigns to deliver personalized offers"},
+    {index: 4, content: "24x7 instant engagement with no-code chatbots"},
+]
