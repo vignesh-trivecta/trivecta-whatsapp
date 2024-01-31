49459253db930f74e97fccb5159aedaee2a8aeae
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-01-31 15:57:48 +0530

    images moved to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     <div className='bg-[#F6F9F9] h-[100vh] overflow-auto p-2'>
       <BrowserRouter>
         <div className='p-2 flex'>
-          <img src='src\assets\logo.jpg' width={'100px'}  height={'80px'} className='mb-2' />
-          {/* <img src='src\assets\logo1.png' width={'120px'}  height={'120px'} className='mb-4 ' /> */}
+          <img src='public\logo.png' width={'100px'}  height={'80px'} className='mb-2' />
+          {/* <img src='public\logo1.png' width={'120px'}  height={'120px'} className='mb-4 ' /> */}
         </div>
         <Routes>
           <Route path='/' element={<Home />} />
