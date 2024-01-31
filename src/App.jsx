5c6f77066a755129c47f64475c6f5da24151fef7
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-01-31 16:00:45 +0530

    logo route changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className='bg-[#F6F9F9] h-[100vh] overflow-auto p-2'>
       <BrowserRouter>
         <div className='p-2 flex'>
-          <img src='public\logo.png' width={'100px'}  height={'80px'} className='mb-2' />
+          <img src={"/logo.png"} width={'100px'}  height={'80px'} className='mb-2' />
           {/* <img src='public\logo1.png' width={'120px'}  height={'120px'} className='mb-4 ' /> */}
         </div>
         <Routes>
